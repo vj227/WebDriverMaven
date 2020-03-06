1403c270d2607444584b71c3c690f07620e15f77
Admin <vj@gmail.com>
2020-03-06 11:35:32 +0530

    this is my first commit

---
+++ Seleniumqspd/src/test/java/org/com/learningMaven/AppTest.java
@@ -0,0 +1,15 @@
+package org.com.learningMaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+	 @Test
+	 public void testHelloWorld()
+	 {
+		 System.out.println("..Hello World..");
+	 }	
+}
+    
